072c0b119504e7436903799989dac9ea725394d1
Shawn <shawn@practera.com>
2019-04-06 18:07:40 +1100

    logic done for setting up board

--- src/app/game/game.page.ts
+++ src/app/game/game.page.ts
@@ -11,8 +11,8 @@ export class GamePage {
   myShipsPosition: Array<Array<boolean>>;
   opponentShipsPosition: Array<Array<boolean>>;
   // These array only contain ship positions
-  myShips: Array<Array<number>>;
-  opponentShips: Array<Array<number>>;
+  myShips: Array<Array<Array<number>>>;
+  opponentShips: Array<Array<Array<number>>>;
   // The positions that are marked as ship when user set up the board
   markedPositions: Array<Array<number>>;
   // current board status
@@ -91,7 +91,6 @@ export class GamePage {
             this.markedPositions.splice(p, 1);
           }
         }
-        console.table(this.markedPositions);
         this._checkSetUpStatus();
         break;
 
@@ -99,35 +98,107 @@ export class GamePage {
   }
 
   /**
-   * Check if any ships is completed and if it is ready to start the game
+   * Start the game after setting up my board
+   */
+  start() {
+    this.state = 'playing';
+  }
+
+  /**
+   * Check if any ships are completed and if it is ready to start the game
    * Algorithm:
    * Connected-component labeling algorithm -> One component at a time
    * (https://en.wikipedia.org/wiki/Connected-component_labeling)
    */
   private _checkSetUpStatus() {
-    const ships = [];
-    const tmpStack = [];
-    const checkedPositions = [];
+    // array of ships positions
+    const ships: Array<Array<Array<number>>> = [];
+    // temporary stack used to group positions to ships
+    const tmpStack: Array<Array<number>> = [];
+    // the positions that has already been checked(labeled - in the algorithm)
+    const checkedPositions: Array<Array<number>> = [];
     this.markedPositions.forEach(position => {
-      if (checkedPositions.includes(position)) {
+      if (this._positionIncludes(checkedPositions, position)) {
         return ;
       }
-      const ship = [];
+      const ship: Array<Array<number>> = [];
       tmpStack.push(position);
       checkedPositions.push(position);
       ship.push(position);
+      // keep checking neighbours of positions inside the stack until stack is empty
       while (tmpStack[0]) {
         const tmpPosition = tmpStack.pop();
         const [x, y] = tmpPosition;
+        // check if the neighbour of this position is marked
+        // if so, they belongs to the same ship
+        // push it to the stack
         [[x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1]].forEach(neighbour => {
-          if (this.markedPositions.includes(neighbour) && !checkedPositions.includes(neighbour)) {
+          if (this._positionIncludes(this.markedPositions, neighbour) &&
+            !this._positionIncludes(checkedPositions, neighbour)) {
             tmpStack.push(neighbour);
             checkedPositions.push(neighbour);
-            ship.push(position);
+            ship.push(neighbour);
           }
         });
       }
-
+      ships.push(ship);
     });
+    // now marked positions on the board are grouped as "ships"
+    // need to validate those ships
+    const validShips: Array<Array<Array<number>>> = [];
+    ships.forEach(ship => {
+      if (ship.length < 2 || ship.length > 5) {
+        return ;
+      }
+      const [x, y] = ship[0];
+      const xEquals = ship.filter(v => {
+        return v[0] === x;
+      });
+      const yEquals = ship.filter(v => {
+        return v[1] === y;
+      });
+      if (xEquals.length === ship.length || yEquals.length === ship.length) {
+        validShips.push(ship);
+      }
+    });
+    // now check which ship has been set up already
+    // initialise all ships' done status to false
+    this.ships = this.ships.map(v => {
+      v.done = false;
+      return v;
+    });
+    // initialise ready to start as false
+    this.readyToStart = false;
+    validShips.forEach(ship => {
+      const index = this.ships.findIndex(currentShip => {
+        return currentShip.size === ship.length && !currentShip.done
+      });
+      if (index !== -1) {
+        this.ships[index].done = true;
+      }
+    });
+    if (!this.ships.find(v => {
+      return !v.done;
+    }) && validShips.length === this.ships.length) {
+      this.myShips = validShips.sort((a, b) => {
+        return b.length - a.length
+      });
+      this.readyToStart = true;
+    }
+  }
+
+  /**
+   * Check if an position array includes the position passed in
+   * @param {Array<Array<number>>} positionArray [The position array]
+   * @param {Array<number>}        position      [The position to check]
+   */
+  private _positionIncludes(positionArray: Array<Array<number>>, position: Array<number>) {
+    let result = false;
+    positionArray.forEach(p => {
+      if (p[0] === position[0] && p[1] === position[1]) {
+        result = true;
+      }
+    });
+    return result;
   }
 }
